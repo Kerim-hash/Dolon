5badaae9d479b4718a6efa1e1777ae4d09e022d9
unknown <kerimnurkaliev3@gmail.com>
2023-07-10 12:41:51 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,13 @@ import Contact from "@/sections/Contact";
 import Hero from "@/sections/Hero";
 import Reviews from "@/sections/Reviews";
 import Modules from "@/sections/modules";
-import CustomCursor from "@/CustomCursor";
+
 import React, { useEffect, useRef, useState } from "react";
 
 
 const Home = () => {
   return (
     <>
-      <CustomCursor />
       <Navbar />
       <Hero />
       <Advantages  />
