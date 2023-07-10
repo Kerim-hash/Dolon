0951cd5c18396b6ab25381871b67ff59586394cd
unknown <kerimnurkaliev3@gmail.com>
2023-07-10 11:59:47 +0300

    added google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import "locomotive-scroll/src/locomotive-scroll.scss";
 
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => (
@@ -15,8 +16,10 @@ const MyApp = ({ Component, pageProps }) => (
         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
       />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css" /> 
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css" />
     </Head>
+
+    <GoogleAnalytics measurementId="G-93SP47C5KR" />
     <Component {...pageProps} />
   </>
 );
